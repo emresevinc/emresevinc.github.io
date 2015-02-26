3717be1c0daa6444c3e0b6d32a0aac316daed9bc
emresevinc <emresevinc10@gmail.com>
2015-02-26 17:58:52 +0200

    %

--- js/forContactHTML.js
+++ js/forContactHTML.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
         $("#divContact").animate({
-            left: '15%',
+            left: '5%',
             height: '15%',
             width: '100%'
         });
